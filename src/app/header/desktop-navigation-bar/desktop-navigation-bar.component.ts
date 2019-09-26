f7fe9443a7e0b5a2de3e008eb78519c185bec2d2
Alexander Sels <alexandersels@hotmail.com>
2019-09-26 20:17:10 +0200

    first commit

---
+++ src/app/header/desktop-navigation-bar/desktop-navigation-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop-navigation-bar',
+  templateUrl: './desktop-navigation-bar.component.html',
+  styleUrls: ['./desktop-navigation-bar.component.scss']
+})
+export class DesktopNavigationBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/header/mobile-navigation-bar/mobile-navigation-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-navigation-bar',
+  templateUrl: './mobile-navigation-bar.component.html',
+  styleUrls: ['./mobile-navigation-bar.component.scss']
+})
+export class MobileNavigationBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
